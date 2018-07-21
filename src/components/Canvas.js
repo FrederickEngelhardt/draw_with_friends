d8ea86d1375dfa616ce09ffca89d8f8b2a8e13ba
FrederickEngelhardt <fre1994@gmail.com>
2018-07-20 21:16:52 -0700

    sass recompile and change from view to client x & y

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -17,23 +17,19 @@ class Canvas extends React.Component {
       canvasWidth: 500,
       canvasHeight: 500,
     }
-    socket.on('update_session_canvas', (data) => {
-      const canvas = this.refs.canvas,
-            canvasBounds = canvas.getBoundingClientRect(),
-            offsetLeft = canvasBounds.left,
-            offsetTop = canvasBounds.top,
-            ctx=canvas.getContext("2d");
-
-      ctx.fillStyle="#FF0000";
-      data.map((element)=>{
-        return ctx.fillRect(...element)})
-      ctx.fillRect(this.state.x,this.state.y,10,10)
-    })
+    // socket.on('update_session_canvas', (data) => {
+    //   const canvas = this.refs.canvas,
+    //         canvasBounds = canvas.getBoundingClientRect(),
+    //         offsetLeft = canvasBounds.left,
+    //         offsetTop = canvasBounds.top,
+    //         ctx=canvas.getContext("2d");
+    //   ctx.fillStyle="#FF0000";
+    //   data.map((element)=>{
+    //     return ctx.fillRect(...element)})
+    //   ctx.fillRect(this.state.x,this.state.y,10,10)
+    // })
     socket.on('load_canvas', (data) => {
       const canvas = this.refs.canvas,
-            canvasBounds = canvas.getBoundingClientRect(),
-            offsetLeft = canvasBounds.left,
-            offsetTop = canvasBounds.top,
             ctx=canvas.getContext("2d");
             ctx.fillStyle="#FF0000";
       return data.map((ele)=>{return ctx.fillRect(...ele)})
@@ -43,10 +39,10 @@ class Canvas extends React.Component {
     // Set the height of the canvas based on the smallest screen dimension size.
     const canvasWidth = window.innerHeight > window.innerWidth ? window.innerWidth*.9 : window.innerHeight*.9,
           canvasHeight = window.innerHeight > window.innerWidth ? window.innerWidth*.9 : window.innerHeight*.9
-
+    console.log(this.refs.canvas.getBoundingClientRect());
     this.setState({
       canvasWidth: canvasWidth,
-      canvasHeight: canvasHeight
+      canvasHeight: canvasHeight,
     })
   }
   _onMouseMove(e) {
@@ -57,7 +53,7 @@ class Canvas extends React.Component {
           offsetTop = canvasBounds.top
 
     // Set the State of the current coordinates
-    this.setState({ x: (e.screenX-offsetLeft), y: (e.screenY-offsetTop) });
+    this.setState({ x: (e.clientX-offsetLeft), y: (e.clientY-offsetTop) });
     if (this.state.x === undefined || this.state.y === undefined) return
 
     let ctx=canvas.getContext("2d");
@@ -65,6 +61,10 @@ class Canvas extends React.Component {
     ctx.fillRect(this.state.x,this.state.y,10,10)
 
     socket.emit('update_canvas', [this.state.x, this.state.y, this.state.rectangle_width, this.state.rectangle_height])
+  }
+  _clearCanvas() {
+    this.refs.canvas.getContext("2d").clearRect(0, 0, this.state.canvasWidth, this.state.canvasHeight)
+    socket.emit("clear_canvas")
   }
   render() {
     return(
@@ -75,6 +75,7 @@ class Canvas extends React.Component {
           width={this.state.canvasWidth}
           onMouseMove={this._onMouseMove.bind(this)}
           ref="canvas"/>
+          <button onClick={this._clearCanvas.bind(this)}>CLICK ME</button>
       </div>
     )
   }
