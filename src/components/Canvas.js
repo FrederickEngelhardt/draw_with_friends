fc3f31e501b574f098d7ab8086ef461ca4f5c1ed
feng <fre1994@gmail.com>
2018-07-25 22:53:31 -0700

    now uses paintbrush svg built by Steve

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -119,13 +119,14 @@ export default class Canvas extends React.Component {
   }
 
   render() {
+    // let cursorStyle = cursor: `url('data:image/svg+xml;utf8,<svg fill="blue" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" stroke="${this.state.selected_color}" d="M18.247 6.763c-1.215 2.033-2.405 4.002-3.056 5.023-.82 1.287-1.102 1.471-2.748 2.142l-.173-.145c.363-1.752.491-2.063 1.606-3.106.882-.823 2.598-2.351 4.371-3.914zm-10.269 11.254l.871.727c-.16 2.549-2.361 3.452-4.231 3.222 1.736-1.604 1.924-3.639 3.36-3.949zm14.746-17.831s-7.887 6.857-10.213 9.03c-1.838 1.719-1.846 2.504-2.441 5.336l2.016 1.681c2.671-1.099 3.44-1.248 4.793-3.373 1.713-2.687 7.016-11.698 7.016-11.698.422-.748-.516-1.528-1.171-.976zm-14.098 15.767c-5.093.053-3.121 5.901-8.626 5.445 3.252 4.523 11.38 3.041 10.832-3.604l-2.206-1.841z"/></svg>') 0 40, auto`
     return(
       <div style={{height: '100vh', backgroundColor: 'blue'}}>
+        <svg className="umbrella" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 400 400" aria-labelledby="title">
+          <path d="M315 10a34 34 0 0 0-48 0l-97 107-31-31a23 23 0 0 0-33 0l-50 51a6 6 0 0 0 0 8l2 3-56 56a6 6 0 0 0 0 9l44 44 66 66a6 6 0 0 0 9 0l56-56 3 2a6 6 0 0 0 8 0l51-50c9-9 9-24 0-33l-31-31 107-97c13-13 13-35 0-48zM116 311l-13-14 23-23a6 6 0 0 0-8-9l-24 23-13-13 23-23a6 6 0 1 0-9-9l-23 23-13-13 23-23a6 6 0 0 0-9-9l-23 23-13-13 22-23a6 6 0 0 0-8-9l-23 23-14-13 53-53 102 102-53 53zm114-101l-46 47L68 141l47-46c2-2 5-4 8-4s6 2 8 4l35 35 29 29 35 35c5 5 5 12 0 16zm77-161l-107 98-22-22 98-107c4-4 9-6 15-6s12 2 16 6c8 9 8 23 0 31z"/>
+        </svg>
         <canvas
-          style={{
-            cursor: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="${this.state.selected_color}" height="48" viewBox="0 0 24 24" width="48"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>'), auto`}
-          }
-
+          style={{cursor: `url('data:image/svg+xml;utf8,<svg fill="blue" xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 500 500">            <path id="Rectangle" fill="${this.state.selected_color}" transform="rotate(45 91 234)" d="M19 194h145v80H19z"/><path fill="black" stroke="black" d="M315 10a34 34 0 0 0-48 0l-97 107-31-31a23 23 0 0 0-33 0l-50 51a6 6 0 0 0 0 8l2 3-56 56a6 6 0 0 0 0 9l44 44 66 66a6 6 0 0 0 9 0l56-56 3 2a6 6 0 0 0 8 0l51-50c9-9 9-24 0-33l-31-31 107-97c13-13 13-35 0-48zM116 311l-13-14 23-23a6 6 0 0 0-8-9l-24 23-13-13 23-23a6 6 0 1 0-9-9l-23 23-13-13 23-23a6 6 0 0 0-9-9l-23 23-13-13 22-23a6 6 0 0 0-8-9l-23 23-14-13 53-53 102 102-53 53zm114-101l-46 47L68 141l47-46c2-2 5-4 8-4s6 2 8 4l35 35 29 29 35 35c5 5 5 12 0 16zm77-161l-107 98-22-22 98-107c4-4 9-6 15-6s12 2 16 6c8 9 8 23 0 31z"/></svg>') 0 40, auto`}}
           className="canvas"
           height={this.state.canvasHeight}
           width={this.state.canvasWidth}
