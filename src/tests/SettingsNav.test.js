a321ed42d97722f51a5fccca46d2bd0694ecb8f0
feng <fre1994@gmail.com>
2018-08-10 09:46:27 -0700

    added setProps to first test

--- src/tests/SettingsNav.test.js
+++ src/tests/SettingsNav.test.js
@@ -7,9 +7,11 @@ import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe("SettingsNav", () => {
+describe("<SettingsNav />", () => {
   it('allows us to set props', () => {
     const wrapper = mount(<SettingsNav settingSelector={()=> true} />)
     expect(wrapper.props().settingSelector()).to.equal(true)
-  });
+    wrapper.setProps({ settingSelector: ()=>{return true} })
+    expect(wrapper.props().settingSelector()).to.equal(true)
+  })
 });
