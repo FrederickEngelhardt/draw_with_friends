17f250a3596f0fbd20fcd31e2f3418b14a7a14b3
feng <fre1994@gmail.com>
2018-08-15 16:27:44 -0700

    rename of file and now pulls redux state needed for homepage components

---
+++ src/containers/HomePageContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as UserActionCreators from '../actions/user';
+
+
+// Stylesheets
+import '../css/App.css'
+import '../css/HomePage.css'
+
+import NavigationContainer from './NavigationContainer'
+import NewSessionCard from '../components/HomePage/NewSessionCard'
+import JoinSessionCard from '../components/HomePage/JoinSessionCard'
+import RecentSessionCard from '../components/HomePage/RecentSessionCard'
+
+class HomePageContainer extends Component {
+  render() {
+    const { user } = this.props;
+    const { sessions } = user
+    return (
+      <div className="App">
+        <NavigationContainer view={`home-view`}/>
+        <div className={`click home-flex`}>
+          <NewSessionCard sessions={sessions}/>
+          <JoinSessionCard />
+          <RecentSessionCard />
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => (
+  {
+    user: state
+  }
+);
+export default connect(mapStateToProps)(HomePageContainer);
