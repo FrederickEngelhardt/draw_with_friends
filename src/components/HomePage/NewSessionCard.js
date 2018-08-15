c068afd95a4f8922ab945235cf0232e6f772993e
feng <fre1994@gmail.com>
2018-08-14 21:53:58 -0700

    form added when you click the plus/button

--- src/components/HomePage/NewSessionCard.js
+++ src/components/HomePage/NewSessionCard.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import { Link, Redirect, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 
 
 export default class NewSessionCard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      form: false,
+    }
+  }
   openMenu(){
+    this.setState({form: true})
+  }
+  handleSubmit(event){
+    event.preventDefault()
+  }
+  renderForm(){
+    if (this.state.form === true){
+      return (
+        <form className={`new-session-form`} onSubmit={this.handleSubmit.bind(this)}>
+          <h4 className={`buttonTitle`}>
+            Add a name to make your session standout!
+          </h4>
+          <input type="text" name="name" />
+          <input type="submit" value="Submit" />
+        </form>
+      )
+    }
+    else {
+      return(
+        <h1 className={`clickAnimation buttonTitle extra-large-font`}> + </h1>
+      )
+    }
   }
   render(){
     return (
-      <div className={`clickAnimation DrawingPageCard`}>
-      <Link className={`DrawingPageCard`} to="/drawing">
-      <button className={`shadow Button green`} onClick={this.openMenu()}></button>
-      <h1 className={`buttonTitle extra-large-font`}> + </h1>
-      </Link>
+      <div className={`DrawingPageCard`}>
+      <button className={`shadow Button green DrawingPageCard`} onClick={this.openMenu.bind(this)}>
+        {this.renderForm()}
+      </button>
       </div>
     )
   }
 }
+const Button = withRouter(({ history }) => (
+  <button
+    type='button'
+    onClick={() => { history.push('/drawing') }}
+  >
+    Click Me!
+  </button>
+))
